914aa7f47ff7c703f1e7b6354a00e9cac8172458
Abhishek <himansupal1998@gmail.com>
2020-05-15 12:58:18 +0000

    Adding react project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Counters from './components/Counters'
+import './App.css';
+
+class App extends Component {
+	state={
+		counters: [
+			{id: 1, value: 4},
+			{id: 2, value: 0},
+			{id: 3, value: 0},
+			{id: 4, value: 0}
+		]
+	};
+
+	handleReset=()=>{
+		const counters=this.state.counters.map(c=>{
+			c.value=0;
+			return c;
+		});
+		this.setState({counters});
+	};
+		
+	handleIncrement=counter=>{
+		const counters=[...this.state.counters];
+		const index=counters.indexOf(counter);
+		counters[index]={...counter};
+		counters[index].value++;
+		this.setState({counters});
+	};
+
+	handleDelete=(counterId)=>{
+		const counters=this.state.counters.filter(c=>c.id!==counterId);
+		this.setState({counters:counters});
+	};
+  	render() {
+    return (
+     	<React.Fragment>
+			<Navbar totalCounters={this.state.counters.filter(c=>c.value>0).length}/>
+			<main className="container">
+				<Counters 
+					counters={this.state.counters}
+					onIncrement={this.handleIncrement}
+					onDelete={this.handleDelete}
+					onReset={this.handleReset}/>
+			</main>
+		</React.Fragment>
+    );
+  }
+}
+
+export default App;
